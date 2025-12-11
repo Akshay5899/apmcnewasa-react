1cdbcc15224b7579c6e0ff461c6ee1f81d6c98bc
sagar jadhav <sagar.jadhav@brawizz.com>
2025-12-11 19:05:40 +0530

    changes 2 files

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export',                // required for GitHub Pages
   images: {
-    unoptimized: true,
+    unoptimized: true,            // disable Next.js image optimization
   },
+  assetPrefix: '/apmcnewasa-react/', // REQUIRED for images, CSS, JS
+  basePath: '/apmcnewasa-react',     // REQUIRED for routing
 };
 
 module.exports = nextConfig;
