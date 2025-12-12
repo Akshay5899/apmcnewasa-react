a7566b11e1d55212ce78744adfe961f8394362a4
sagar jadhav <sagar.jadhav@brawizz.com>
2025-12-12 16:53:09 +0530

    change next file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: { unoptimized: true },
   basePath: '/apmcnewasa-react',
   assetPrefix: '/apmcnewasa-react/',
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
